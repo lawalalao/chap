513aa4a686875fd8183a84f4c8948e4c7403230e
lawalalao <lawalalaoad@gmail.com>
2020-11-12 22:12:32 +0100

    Add files via upload
    update mobile view

--- app.js
+++ app.js
@@ -69,20 +69,6 @@ function init() {
 }
 
 
-var prevScrollpos = window.pageYOffset;
-      window.onscroll = function() {
-
-      var currentScrollpos = window.pageYOffset;
-      if(prevScrollpos > currentScrollpos) {
-            document.getElementById("navbar").style.top = "0";
-            
-      } else {
-            document.getElementById("navbar").style.top = "-100";
-      }
-
-      prevScrollpos = currentScrollpos;
-
-      }
 const countdown = document.querySelector(".countdown");
 
 const interval = setInterval(() => {
